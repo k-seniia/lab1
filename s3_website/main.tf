9b872b8ab7e628212a7d66369ab9d34e38cb6047
_k.senia_ <73493801+k-seniia@users.noreply.github.com>
2024-06-08 22:08:39 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -28,6 +28,22 @@ resource "aws_s3_bucket" "website" {
 resource "aws_s3_bucket_acl" "example_acl" {
 	bucket = aws_s3_bucket.website.id
 	acl    = "public-read"
+	depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
