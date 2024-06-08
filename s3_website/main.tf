eb87aad9fe01f1f59c3f26c6c92430900bb33d63
_k.senia_ <73493801+k-seniia@users.noreply.github.com>
2024-06-08 22:13:32 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -1,33 +1,29 @@
 terraform {
 	required_providers {
 		aws = {
-			source 	= "hashicorp/aws"
-			version = "~> 4.0"
+		source = "hashicorp/aws"
+		version = "~> 4.0"
 		}
 	}
 }
-
 # Configure AWS provider and creds
 provider "aws" {
 	region = "us-east-1"
 	shared_config_files = ["config"]
-	shared_credentials_files = ["credentials"]
+	shared_credentials_files = ["credentials"] 
 	profile = "default"
 }
-
 # Creating bucket
 resource "aws_s3_bucket" "website" {
 	bucket = "hetman-bucket-54151351561556"
-	
 	tags = {
-		Name 		= "Website"
+		Name = "Website"
 		Environment = "Dev"
 	}
 }
-
 resource "aws_s3_bucket_acl" "example_acl" {
 	bucket = aws_s3_bucket.website.id
-	acl    = "public-read"
+	acl = "public-read"
 	depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
@@ -48,44 +44,34 @@ resource "aws_s3_bucket_public_access_block" "example" {
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
 	bucket = aws_s3_bucket.website.id
-	
 	index_document {
 		suffix = "index.html"
 	}
-	
 	error_document {
 		key = "error.html"
 	}
 }
-
 resource "aws_s3_bucket_policy" "allow_access" {
 	bucket = aws_s3_bucket.website.id
 	policy = data.aws_iam_policy_document.allow_access.json
 }
-
 data "aws_iam_policy_document" "allow_access" {
 	statement {
 		principals {
-			type 		= "AWS"
+			type = "AWS"
 			identifiers = ["*"]
 		}
-		actions = [
-			"s3:GetObject"
-		]
-		resources = [
-			aws_s3_bucket.website.arn,
-			"${aws_s3_bucket.website.arn}/*",
-		]
+		actions = [ "s3:GetObject" ]
+		resources = [ aws_s3_bucket.website.arn,
+		"${aws_s3_bucket.website.arn}/*", ]
 	}
 }
-
 resource "aws_s3_object" "indexfile" {
-	bucket 		 = aws_s3_bucket.website.id
-	key 		 = "index.html"
-	source 		 = "./src/index.html"
+	bucket = aws_s3_bucket.website.id
+	key = "index.html"
+	source = "./src/index.html"
 	content_type = "text/html"
 }
-
 output "website_endpoint" {
 	value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
