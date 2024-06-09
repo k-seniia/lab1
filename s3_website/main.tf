714c5ae9ee49c2fc11fef2ecd99012d35710c67d
_k.senia_ <73493801+k-seniia@users.noreply.github.com>
2024-06-09 03:07:00 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -1,11 +1,18 @@
 terraform {
+  backend "s3" {
+    bucket         = "hetman-terraform-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraformbackendtable"
+  }
 	required_providers {
 		aws = {
-		source = "hashicorp/aws"
-		version = "~> 4.0"
+			source 	= "hashicorp/aws"
+			version = "~> 4.0"
 		}
 	}
 }
+
 # Configure AWS provider and creds
 provider "aws" {
 	region = "us-east-1"
